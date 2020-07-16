f5e9bf9ce2cdb9a6a6e22cd87beb97ba143a9c3c
634597074 <m15626272182@163.com>
2020-07-16 17:04:35 +0800

    第一次创建

---
+++ cmake-build-debug/CMakeFiles/ZPAQhit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ZPAQhit.dir/main.cpp.o"
+  "ZPAQhit.pdb"
+  "ZPAQhit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZPAQhit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
